603935e537da873798d6f19ba07286a0d2de430e
Echeverria <ue010006ry@baustro.fin.ec>
2025-06-23 16:53:31 -0500

    Mejora del Formato del Documento

--- data/pdf_generator.py
+++ data/pdf_generator.py
@@ -11,9 +11,10 @@ from reportlab.lib.units import cm
 
 from reportlab.platypus import Preformatted, XPreformatted
 from reportlab.lib.styles import ParagraphStyle
-from reportlab.lib.enums import TA_LEFT
+from reportlab.lib.enums import TA_LEFT, TA_CENTER
 import textwrap
 import re
+
 
 class PDFGenerator:
 
@@ -23,52 +24,85 @@ class PDFGenerator:
             pagesize=A4,
             rightMargin=2*cm,
             leftMargin=2*cm,
-            topMargin=2*cm,
+            topMargin=3*cm,
             bottomMargin=2*cm        
             )
         self.estilos = getSampleStyleSheet()
         self.contenido = []
 
-    def AddHeader(self, canvas, doc):
-        try:
-            canvas.saveState()
+    # def AddHeader(self, canvas, doc):
+    #     try:
+    #         canvas.saveState()
 
-            # Ruta y tamaño ajustado de la imagen
-            logo_path = "data/assets/LOGOBA.png" 
-            logo_width = 110   # Aumentado de 80
-            logo_height = 18   # Aumentado de 15
+    #         # Ruta y tamaño ajustado de la imagen
+    #         logo_path = "data/assets/LOGOBA.png" 
+    #         logo_width = 110   # Aumentado de 80
+    #         logo_height = 18   # Aumentado de 15
 
-            y_position = A4[1] - 60  # Más abajo que antes (antes era -40)
+    #         y_position = A4[1] - 60  # Más abajo que antes (antes era -40)
 
-            # Dibujar logo
-            canvas.drawImage(
-                logo_path,
-                doc.leftMargin,
-                y_position,
-                width=logo_width,
-                height=logo_height,
-                mask='auto'
+    #         # Dibujar logo
+    #         canvas.drawImage(
+    #             logo_path,
+    #             doc.leftMargin,
+    #             y_position,
+    #             width=logo_width,
+    #             height=logo_height,
+    #             mask='auto'
+    #         )
+
+    #         # Dibujar título centrado verticalmente con respecto al logo
+    #         canvas.setFont("Helvetica-Bold", 12)
+    #         canvas.drawRightString(
+    #             A4[0] - doc.rightMargin,
+    #             y_position + logo_height / 4 + 2,
+    #             "INFORME TÉCNICO DEL SISTEMA"
+    #         )
+
+    #         canvas.restoreState()
+    #     except Exception as e:
+    #         print(f"[Error en encabezado]: {e}")
+
+    
+    def agregar_titulo_centrado(self, texto):
+            estilo_titulo = ParagraphStyle(
+                name="TituloCentrado",
+                fontName="Courier-Bold",
+                fontSize=13,
+                alignment=TA_CENTER,
+                spaceAfter=12
             )
+            self.contenido.append(Paragraph(texto, estilo_titulo))
 
-            # Dibujar título centrado verticalmente con respecto al logo
-            canvas.setFont("Helvetica-Bold", 12)
-            canvas.drawRightString(
-                A4[0] - doc.rightMargin,
-                y_position + logo_height / 4 + 2,
-                "INFORME TÉCNICO DEL SISTEMA"
-            )
+    def AddParagraph(self, texto, titulo="Datos del Usuario"):
+        estilo_titulo = ParagraphStyle(
+            name="TituloUsuario",
+            fontName="Courier-Bold",
+            fontSize=11,
+            leading=14,
+            leftIndent=10,     
+            spaceBefore=10,
+            spaceAfter=2,
+            alignment=TA_LEFT
+        )
 
-            canvas.restoreState()
-        except Exception as e:
-            print(f"[Error en encabezado]: {e}")
+        estilo_html = ParagraphStyle(
+            name="TextoHTML",
+            fontSize=9,
+            fontName="Courier",
+            leading=11,
+            leftIndent=10,       
+            spaceBefore=0,
+            spaceAfter=1,
+            alignment=TA_LEFT
+        )
 
+        # Añade el título como un párrafo
+        self.contenido.append(Paragraph(titulo, estilo_titulo))
 
-    def AddParagraph(self, texto, titulo = "Datos del Usuario"):
-        self.contenido.append(Paragraph(f"<b>{titulo}</b>", self.estilos["Heading2"]))
-        for line in texto.split("\n"):
-            if line.strip():
-                self.contenido.append(Paragraph(line.strip(), self.estilos["Normal"]))
-        self.contenido.append(Spacer(1, 12))
+        # Añade el contenido HTML con estilo personalizado
+        self.contenido.append(Paragraph(texto, estilo_html))
+        self.contenido.append(Spacer(1, 4))
 
 
     def generar_bloques_formato_fijo(self, secciones: dict):
@@ -107,7 +141,6 @@ class PDFGenerator:
             bloque += '-'*80 + '\n'
             return bloque
 
-        from reportlab.platypus import XPreformatted  # Importar XPreformatted que sí acepta HTML simple
         for titulo, campos in secciones.items():
             bloque_texto = formatear_seccion(titulo, campos)
             self.contenido.append(XPreformatted(bloque_texto, estilo_parrafo))
@@ -208,66 +241,72 @@ class PDFGenerator:
         from reportlab.platypus import XPreformatted
         self.contenido.append(XPreformatted("\n".join(contenido), estilo))
 
-    def AddTable(self, titulo, cabecera, datos):    
-        self.contenido.append(Paragraph(titulo, self.estilos["Title"]))
-        tabla = Table([cabecera] + datos, repeatRows=1)
-        tabla.setStyle(TableStyle([
-            ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-            ('GRID', (0, 0), (-1, -1), 1, colors.black)
-        ]))
-        self.contenido.append(tabla)
+    # def AddTable(self, titulo, cabecera, datos):    
+    #     self.contenido.append(Paragraph(titulo, self.estilos["Title"]))
+    #     tabla = Table([cabecera] + datos, repeatRows=1)
+    #     tabla.setStyle(TableStyle([
+    #         ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+    #         ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+    #         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+    #         ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    #     ]))
+    #     self.contenido.append(tabla)
 
-    def AddKeyValueTable(self, titulo, data_dict):
-        self.contenido.append(Paragraph(titulo, self.estilos["Title"]))
+    # def AddKeyValueTable(self, titulo, data_dict):
+    #     self.contenido.append(Paragraph(titulo, self.estilos["Title"]))
 
-        col_widths = [130, 200] 
+    #     col_widths = [130, 200] 
 
-        data = [[Paragraph(f"<b>{k}</b>", self.estilos["Normal"]), Paragraph(str(v), self.estilos["Normal"])] for k, v in data_dict.items()]
-        tabla = Table(data, colWidths=col_widths)   
+    #     data = [[Paragraph(f"<b>{k}</b>", self.estilos["Normal"]), Paragraph(str(v), self.estilos["Normal"])] for k, v in data_dict.items()]
+    #     tabla = Table(data, colWidths=col_widths)   
 
-        tabla.setStyle(TableStyle([
-            ('GRID', (0, 0), (-1, -1), 0.5, colors.black),
-            ('VALIGN', (0, 0), (-1, -1), 'TOP'),
-            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-            ('BACKGROUND', (0, 0), (0, -1), colors.lightgrey),
-            ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
-        ]))
+    #     tabla.setStyle(TableStyle([
+    #         ('GRID', (0, 0), (-1, -1), 0.5, colors.black),
+    #         ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+    #         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+    #         ('BACKGROUND', (0, 0), (0, -1), colors.lightgrey),
+    #         ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+    #     ]))
 
-        self.contenido.append(tabla)
-        self.contenido.append(Spacer(1, 16))    
+    #     self.contenido.append(tabla)
+    #     self.contenido.append(Spacer(1, 16))    
 
 
-    def AddSmartStatus(self, smart_info):
-        self.contenido.append(Paragraph("Estado SMART de los Discos", self.estilos["Title"]))
+    # def AddSmartStatus(self, smart_info):
+    #     self.contenido.append(Paragraph("Estado SMART de los Discos", self.estilos["Title"]))
 
-        for disk, estado in smart_info.items():
-            if isinstance(estado, dict):
-                self.contenido.append(Paragraph(f"<b>🖴 {disk}</b>", self.estilos["Normal"]))
-                for k, v in estado.items():
-                    self.contenido.append(Paragraph(f"{k}: {v}", self.estilos["Normal"]))
-                self.contenido.append(Spacer(1, 8))
-            else:
-                self.contenido.append(Paragraph(f"🖴 {disk}: No se pudo obtener información SMART.", self.estilos["Normal"]))
-                self.contenido.append(Spacer(1, 8))
+    #     for disk, estado in smart_info.items():
+    #         if isinstance(estado, dict):
+    #             self.contenido.append(Paragraph(f"<b>🖴 {disk}</b>", self.estilos["Normal"]))
+    #             for k, v in estado.items():
+    #                 self.contenido.append(Paragraph(f"{k}: {v}", self.estilos["Normal"]))
+    #             self.contenido.append(Spacer(1, 8))
+    #         else:
+    #             self.contenido.append(Paragraph(f"🖴 {disk}: No se pudo obtener información SMART.", self.estilos["Normal"]))
+    #             self.contenido.append(Spacer(1, 8))
 
 
     def PageFooter(self, canvas, doc):
-            canvas.saveState()
-            canvas.setFont('Helvetica', 8)
-            canvas.drawString(40, 30, "Informe generado automáticamente por el sistema.")
-            canvas.drawRightString(A4[0] - 40, 30, f"Página {canvas.getPageNumber()}")
-            canvas.restoreState()
-    
-    def _withHeaderAndFooter(self, canvas, doc):
-        self.AddHeader(canvas, doc)
-        self.PageFooter(canvas, doc)
+        canvas.saveState()
+        canvas.setFont('Courier-Bold', 8)
+
+        # Usar márgenes definidos en el documento
+        left_margin = doc.leftMargin
+        right_margin = doc.rightMargin
+        page_width = A4[0]
+
+        # Alinear texto izquierdo con el inicio de los cuadros
+        canvas.drawString(left_margin, 30, "Informe generado automáticamente por el sistema.")
+
+        # Alinear número de página con el final de los cuadros
+        canvas.drawRightString(page_width - right_margin, 30, f"Página {canvas.getPageNumber()}")
+
+        canvas.restoreState()
 
     def SaveTable(self):
             self.pdf.build(
                 self.contenido,
-                onFirstPage = self._withHeaderAndFooter, 
-                onLaterPages=self._withHeaderAndFooter)
+                onFirstPage = self.PageFooter, 
+                onLaterPages=self.PageFooter)
 
 
